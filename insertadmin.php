74ce4adc3a5329680e0cb0e919b6a7db52032eb0
padmakar kasture <51418366+padmakar4u@users.noreply.github.com>
2020-01-10 14:02:14 +0530

    Add files via upload

---
+++ insertadmin.php
@@ -0,0 +1,16 @@
+<?php
+include("connect.php");
+$drop="drop table tokendb";
+$drp=mysqli_query($conn,$drop);
+$sql="CREATE TABLE tokendb(
+    tokenno int NOT NULL AUTO_INCREMENT,
+    deposit int NOT NULL,
+    withdraw int  NOT NULL,
+    other int NOT NULL,
+    PRIMARY KEY(tokenno)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+  $res=mysqli_query($conn,$sql);
+  if($res){
+      echo "kdk";
+  }
+?>
